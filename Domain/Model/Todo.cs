724f9a35bb17a7ab2d857f4182071522051f58ea
Himal sharma <41054866+himal28924@users.noreply.github.com>
2022-03-14 16:53:02 +0100

    Done until 21

--- Domain/Model/Todo.cs
+++ Domain/Model/Todo.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Model;
 
 public class Todo
 {
+    public Todo() { }
+
     public int Id { get; set; }
+  
+    
+    [Required, MaxLength(128)] 
     public  string Title { get; set; }
+    
+    [Range(1,int.MaxValue,ErrorMessage = "Please Enter a Value bigger than {1} ")]
     public  int OwnerId { get; set; }
     public bool IsCompleted { get; set; }
 
